8af59c7cc637ac29f1c84ab5684e7380cc18302d
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-05-23 11:53:02 +0200

    Add a settings screen with dummy data

--- app/src/main/java/com/creeper82/pozdroid/ui/common.kt
+++ app/src/main/java/com/creeper82/pozdroid/ui/common.kt
@@ -1,12 +1,22 @@
 package com.creeper82.pozdroid.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun Header(text: String, modifier: Modifier = Modifier) {
     Text(text = text, fontSize = 24.sp, fontWeight = FontWeight.Bold, modifier = modifier)
+}
+
+@Composable
+fun Padding(value: Dp, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Column(modifier = modifier.padding(value)) {
+        content()
+    }
 }
--- app/src/main/java/com/creeper82/pozdroid/ui/screens/MainScreen.kt
+++ app/src/main/java/com/creeper82/pozdroid/ui/screens/MainScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -31,7 +31,7 @@ fun PozDroidApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-    var showIntro by remember { mutableStateOf(true) }
+    var showIntro by rememberSaveable { mutableStateOf(true) }
     val screenModifier = Modifier
         .fillMaxSize()
         .padding(16.dp)
@@ -69,6 +69,11 @@ fun PozDroidApp(
                     modifier = modifier.fillMaxSize(),
                     onBollardSelected = {},
                     onLineSelected = {}
+                )
+            }
+            composable(route = PozDroidScreen.Settings.name) {
+                PozDroidSettingsScreen(
+                    modifier = modifier.fillMaxSize()
                 )
             }
         }
