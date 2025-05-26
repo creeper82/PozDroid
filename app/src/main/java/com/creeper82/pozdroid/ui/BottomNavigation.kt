1a9bc86a7097ae2557aa0e7f00d65776b10b222f
creeper82 <66107554+creeper82@users.noreply.github.com>
2025-05-26 09:01:46 +0200

    Fix navigation state sync

--- app/src/main/java/com/creeper82/pozdroid/ui/BottomNavigation.kt
+++ app/src/main/java/com/creeper82/pozdroid/ui/BottomNavigation.kt
@@ -10,13 +10,12 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.creeper82.pozdroid.ui.screens.PozDroidScreen
 
 data class TopLevelRoute<T : Any>(val label: String, val route: T, val icon: ImageVector)
@@ -25,21 +24,21 @@ data class TopLevelRoute<T : Any>(val label: String, val route: T, val icon: Ima
 val bottomNavItems = arrayOf<TopLevelRoute<String>>(
     TopLevelRoute("Home", PozDroidScreen.Home.name, Icons.Filled.Home),
     TopLevelRoute("Search", PozDroidScreen.Search.name, Icons.Filled.Search),
-    TopLevelRoute("More", PozDroidScreen.Settings.name, Icons.Filled.Menu),
+    TopLevelRoute("More", PozDroidScreen.Settings.name, Icons.Filled.Menu)
 )
 
 @Composable
 fun PozDroidBottomNav(navController: NavController, modifier: Modifier = Modifier) {
-    var selectedIndex by remember { mutableIntStateOf(0) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar {
+    NavigationBar(modifier = modifier) {
         bottomNavItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = { Icon(item.icon, contentDescription = item.label) },
                 label = { Text(item.label) },
-                selected = selectedIndex == index,
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
                 onClick = {
-                    selectedIndex = index
                     navController.navigate(item.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
